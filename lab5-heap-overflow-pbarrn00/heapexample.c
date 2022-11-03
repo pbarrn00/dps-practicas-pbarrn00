8954bcc4a90a0c7ce7ad874be5745e0ffd333e43
Pablo <pbarrn00@estudiantes.unileon.es>
2022-11-03 09:23:53 +0100

    Added heap example, ready to start practice!

---
+++ lab5-heap-overflow-pbarrn00/heapexample.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+struct s_data {
+    char buffer[64];
+};
+
+struct s_fp {
+    int (*fp)();
+};
+
+void f_entrar(){
+    printf("Entrando...");
+}
+
+void f_espero_fuera(){
+    printf("Esperando fuera...");
+}
+
+int main(int argc, char **argv)
+{
+    struct s_data *s_midat;
+    struct s_fp *f;
+
+    s_midat = malloc(sizeof(struct s_data));
+    f = malloc(sizeof(struct s_fp));
+    f->fp = f_espero_fuera;
+    printf("data: esta en [%p], el puntero esta en [%p]\n",s_midat, f);
+
+    strcpy(s_midat->buffer, argv[1]);
+    f->fp();
+    
+    return 0;
+}
